4a7a3a415c56c59693af00acedab7b0e43b922c7
Indra Madyasiwi W <madyasiwi@gmail.com>
2020-05-07 18:29:38 +0700

    Add runtime code

---
+++ MyPlatformApp/Packages/MyPlatformSDK/Runtime/Core.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyPlatform {
+    public class Core {
+        /// <summary>
+        /// Initialize my platform.
+        /// </summary>
+        /// <returns>
+        /// True if successful.
+        /// </returns>
+        public static bool Initialize() {
+            return true;
+        }
+    }
+}
