47194ea6d5d0a686d8c60eabc8f79505ef381784
Indra Madyasiwi W <madyasiwi@gmail.com>
2020-05-07 18:30:40 +0700

    Add runtime test code

---
+++ MyPlatformApp/Packages/MyPlatformSDK/Tests/Runtime/CoreTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace MyPlatform.Tests {
+    public class CoreTests {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void InitializationTest() {
+            Assert.True(Core.Initialize());
+        }
+    }
+}
