d547c64895d9d88f759760b14ee1737ffbfff95a
Indra Madyasiwi W <madyasiwi@gmail.com>
2020-05-07 18:49:06 +0700

    Add script to initialize plaform when game starts

---
+++ MyGameApp/Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyPlatform;
+
+
+public class Bootstrap {
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    static void InitializePlatformSDK() {
+        Debug.Log("Initializing platform...");
+        if (Core.Initialize()) {
+            Debug.Log("Success!");
+        }
+    }
+}
